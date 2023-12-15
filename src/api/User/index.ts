83405c7c7c131b48c233647302aeb34dfa59b2a6
Daniel <danielakanbi01@gmail.com>
2023-12-15 14:39:14 +0100

    sorted

--- src/api/User/index.ts
+++ src/api/User/index.ts
@@ -1,7 +1,7 @@
 import { IUser } from "./IUser";
-import { UserConnectionsModel } from "./userConnectionsModel";
+
 import { UserModel } from "./userModel";
 import { UserRouter } from "./userRouter";
 import { UserService } from "./userService";
 
-export { UserModel, IUser, UserRouter, UserService, UserConnectionsModel };
+export { UserModel, IUser, UserRouter, UserService };
